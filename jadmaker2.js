e45093425f610a7a1dc0dded911ce31686bcb069
gtrxAC <gtrxac2@gmail.com>
2025-12-29 20:35:51 +0200

    Default permissions when signing

--- jadmaker2.js
+++ jadmaker2.js
@@ -33,6 +33,14 @@ function createJadFromJar(path, outPath, jarUrl, infoUrl, sign) {
 
         // hardcoded base64 of darkman cert
         mf.set("MIDlet-Certificate-1-1","MIIB7jCCAVcCBEWxvN0wDQYJKoZIhvcNAQEEBQAwPTELMAkGA1UEBhMCUlUxDTALBgNVBAoTBG5vbmUxDTALBgNVBAsTBG5vbmUxEDAOBgNVBAMTB0RhcmttYW4wIBcNMDcwMTIwMDY1NTI1WhgPMjA3NTA3MDIwNjU1MjVaMD0xCzAJBgNVBAYTAlJVMQ0wCwYDVQQKEwRub25lMQ0wCwYDVQQLEwRub25lMRAwDgYDVQQDEwdEYXJrbWFuMIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCYLFUb8RYT89sbTAEE14ApYFI8PVpnxXGgLuE8V6+XGQu4q5MtYwmip8EMm/STLXb73gQmDnQUpwBKzTScXLQDA4n9lLni4yl29/+X5Y0rIA6tlPmK3p9wpt0t9j/rWEYF4zFsiMTNobGHZOK/MAxOM+wPICRW8DFLQ/rYcNjcpQIDAQABMA0GCSqGSIb3DQEBBAUAA4GBAG1fDNKSjvcvvi20AREsMT80iJdO/YXqcVrUsYrQVaZL3scsA+EVKi7Dv76c8oqjxxiueOnn+fTTmlkAOO5ngzZhk13m3tcNxwUs0A/1GBMbVDbYlEc6vEYQde9x+07iyrxmtwj6qVR1r3zTEy2wS52poVmCkcrPXY0wylKesjFP");
+
+        // if there are no permissions defined, define a set of optional ones, so the app will not show permission errors
+        // this is a list of permissions that I consider commonly used, but there are many missing
+        // if your app needs permissions not listed here, you can add MIDlet-Permissions(-Opt) manually
+        // a more complete list is in the MIDP 2.0 specification PDF, page 508
+        if (!mf.has("MIDlet-Permissions") && !mf.has("MIDlet-Permissions-Opt")) {
+            mf.set("MIDlet-Permissions-Opt", "javax.microedition.io.Connector.http, javax.microedition.io.Connector.socket, javax.microedition.io.Connector.file.read, javax.microedition.io.Connector.file.write");
+        }
     }
 
     fs.writeFileSync(outPath, createJad(mf));
